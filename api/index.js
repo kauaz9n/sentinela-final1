8d67446e2f2db291940ec25e8e922177e8439650
kauaz9n <kauazxns@gmail.com>
2026-09-15 09:14:17 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -148,4 +148,8 @@ app.get("/medicacoes", (req, res) => {
 });
 
 // START
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
